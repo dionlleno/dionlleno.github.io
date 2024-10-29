6fd1fe8b93e66a0384f0fc2e7beb70836a03e133
dionlleno <dionlleno@proton.me>
2024-10-29 19:20:58 -0300

    Update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// JavaScript para alternar o menu
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('nav ul').classList.toggle('menu-aberto');
+});
